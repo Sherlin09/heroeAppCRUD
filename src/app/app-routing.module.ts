902e9dcb692d757e0ef963c09b540fc2e53d99c1
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2020-11-13 19:56:40 -0600

    agregar archisvos iniciales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
 
-const routes: Routes = [];
+const ROUTES: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroe/:id', component: HeroeComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'heroes' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot( ROUTES )
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
